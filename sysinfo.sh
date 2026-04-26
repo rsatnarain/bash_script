0c1c74d6385fd56311db3982cc3fe96d62f2a52c
rsatnarain <type.unique_0o@icloud.com>
2026-04-26 18:09:04 -0400

    new file script

---
+++ sysinfo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+report "system_report_$(date +%F).txt
+
+echo "System Report" > "$report"
+echo "------------------------------"
+echo "IP Address: $(hostname -I)" >> "$report"
+echo "Current User: $(whoami)" >> "$report"
+echo "Disk Usage: " >> "$report"
+df -h / >> "$report"
+echo "Data saved to $report"
+echo "-----------------------------"
