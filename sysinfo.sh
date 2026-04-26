7449e648c36c9a16e5391dc2b5b9abdc9da98292
rsatnarain <type.unique_0o@icloud.com>
2026-04-26 18:19:44 -0400

    Fix: Correct variable assignment and improve echo command syntax in sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-report "system_report_$(date +%F).txt"
+report="system_report_$(date +%F).txt"
 
 echo "System Report" > "$report"
 echo "------------------------------" >> "$report"
-echo "IP Address: $(hostname -I)" >> "$report"
-echo "Current User: "$(whoami)" >> "$report"
+
+echo "IP Address: $(hostname)" >> "$report"
+
+echo "Current User: $(whoami)" >> "$report"
+
 echo "Disk Usage: " >> "$report"
 df -h / >> "$report"
-echo "Data saved to $report"
 echo "-----------------------------" >> "$report"
+
+echo "Data saved to $report"
 echo "System report generation completed."
